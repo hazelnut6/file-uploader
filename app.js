7899b4052dd3b9acddc02e3407c104bb71bfecff
hazelnut6 <hazelpachoco6@gmail.com>
2025-07-02 14:13:51 +0800

    Add passport and prisma session store

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 require('dotenv').config();
+const session = require('express-session');
+const passport = require('passport');
+const { PrismaClient } = require('@prisma/client');
+const PrismaSessionStore = require('@quixo3/prisma-session-store')(session, {
+    checkPeriod: 2 * 60 * 1000,
+    dbRecordIdIsSessionId: true,
+    sessionModelName: 'Session',
+    prisma: new PrismaClient(),
+});
+require('./config/passport')(passport);
 const express = require('express');
 const path = require('node:path');
 
@@ -12,6 +22,19 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(
+    session({
+        secret: process.env.SECRET_KEY,
+        resave: false,
+        saveUninitialized: false,
+        store: PrismaSessionStore,
+        cookie: {
+            maxAge: 1000 * 60 * 24
+        }
+    })
+);
+app.use(passport.initialize);
+app.use(passport.session());
 
 // Routes
 app.get('/', (req, res) => {
