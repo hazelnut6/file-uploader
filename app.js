cdf5a169f8e5cc837437427960918dade3ec2c76
hazelnut6 <hazelpachoco6@gmail.com>
2025-07-05 12:57:45 +0800

    Add session store, routes, and global middlewares

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 require('dotenv').config();
 const session = require('express-session');
+const flash = require('connect-flash');
 const passport = require('passport');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
 const { PrismaClient } = require('@prisma/client');
-const PrismaSessionStore = require('@quixo3/prisma-session-store')(session, {
-    checkPeriod: 2 * 60 * 1000,
-    dbRecordIdIsSessionId: true,
-    sessionModelName: 'Session',
-    prisma: new PrismaClient(),
-});
 require('./config/passport')(passport);
 const express = require('express');
 const path = require('node:path');
+const indexRoutes = require('./routes/indexRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
@@ -23,23 +21,37 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(
-    session({
-        secret: process.env.SECRET_KEY,
-        resave: false,
-        saveUninitialized: false,
-        store: PrismaSessionStore,
-        cookie: {
-            maxAge: 1000 * 60 * 24
-        }
-    })
+  session({
+    cookie: {
+     maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+    },
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(
+      new PrismaClient(),
+      {
+        checkPeriod: 2 * 60 * 1000,  //ms
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }
+    )
+  })
 );
-app.use(passport.initialize);
+app.use(flash());
+app.use(passport.initialize());
 app.use(passport.session());
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
+  res.locals.info = req.flash('info');
+  next();
+});
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('index', { title: 'File Uploader' });
-});
+app.use('/', indexRoutes);
+app.use('/auth', authRoutes);
 
 // Port
 const PORT = process.env.PORT || 3000;
@@ -47,6 +59,6 @@ app.listen(PORT, () => { console.log(`Running on port ${PORT}`) });
 
 // Basic error
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
