ec3a6a042e7e894921ac6b3162a232dd4e254694
hazelnut6 <hazelpachoco6@gmail.com>
2025-07-05 14:33:35 +0800

    Create new route file for storage

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,7 +1,24 @@
 const { Router } = require('express');
 const indexRoutes = Router();
 const authController = require('../controllers/authController');
+const indexController = require('../controllers/indexController');
+const multer = require('multer');
+const path = require('node:path');
 
-indexRoutes.get('/', authController.getHome);
+// storage
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'public/uploads/'); // where file uploads will be stored
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); // filename
+    }
+});
+
+// initialize upload middleware
+const upload = multer({ storage: storage });
+
+indexRoutes.get('/', indexController.getHome);
+indexRoutes.post('/upload', authController.isAuthenticated, upload.single('myFile'), indexController.postUpload);
 
 module.exports = indexRoutes;
